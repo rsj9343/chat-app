5ca9e47032d1c94d8d2a576a931d5c80123095d3
Rohitsj627 <rohitsj27@gmail.com>
2025-05-22 03:31:14 +0530

    complete project setup with backend

---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
+import { connectDB } from './lib/db.js';
+
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+app.listen(PORT, () => {
+  console.log('Server is running on port ' + PORT);
+  connectDB();
+});
